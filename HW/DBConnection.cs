916b08ce285bb664b6e86125f94ba66ba84c8964
Satoshi Ookami <lgasparek@cominfo.cz>
2022-02-11 08:46:45 +0100

    Extract DBConnection class

---
+++ HW/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿namespace HW
+{
+    /// <summary>
+    /// Class for carrying database connection 
+    /// </summary>
+    public class DBConnection
+    {
+        public string DBServer { get; set; }
+        public string DBName { get; set; }
+        public string DBUser { get; set; }
+        public string DBPass { get; set; }
+        public bool IntegratedSecurity { get; set; }
+    }
+}
--- HW/MainScreen.cs
+++ HW/MainScreen.cs
@@ -135,16 +135,4 @@ namespace HW
             throw new NotImplementedException();
         }
     }
-
-    /// <summary>
-    /// Class for carrying database connection 
-    /// </summary>
-    public class DBConnection
-    {
-        public string DBServer { get; set; }
-        public string DBName { get; set; }
-        public string DBUser { get; set; }
-        public string DBPass { get; set; }
-        public bool IntegratedSecurity { get; set; }
-    }
 }
